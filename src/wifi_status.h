107b75ad0dc35c2f4a6e795084d8fbdec7f34d2c
banban <banbanhandai@gmail.com>
2025-05-03 18:44:17 +0900

    add host_ip

--- src/wifi_status.h
+++ src/wifi_status.h
@@ -4,3 +4,5 @@
 // please your wifi_status
 const char *ssid = "REPLACE_WITH_YOUR_SSID";
 const char *password = "REPLACE_WITH_YOUR_PASSWORD";
+const char *host_ip = "YOUR_UNITY_PC_IP";
+const int host_port = 8888;
